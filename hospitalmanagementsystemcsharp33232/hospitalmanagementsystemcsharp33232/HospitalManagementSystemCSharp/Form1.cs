e54db257378002b129b01f4902f9008485e70b89
Ahmed <89756008+ahmedhossams@users.noreply.github.com>
2024-04-23 15:33:52 +0200

    Add files via upload

---
+++ hospitalmanagementsystemcsharp33232/hospitalmanagementsystemcsharp33232/HospitalManagementSystemCSharp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace HospitalManagementSystemCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {           
+                if (textBox1.Text == "ahmed" || textBox2.Text == "19104242")
+                {
+                    MessageBox.Show("Welcome DR. You are logged in successfully.");
+                    this.Visible = false;
+                    Home obj1 = new Home();
+                    obj1.ShowDialog();
+                    textBox1.Text = "";
+                    textBox2.Text = "";                 
+                }
+                else
+                {
+                    MessageBox.Show("Error try again .");
+                }
+         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
